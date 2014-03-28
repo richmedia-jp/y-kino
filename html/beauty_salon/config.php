1c8f0c9d8cfe5950c2bfc75d57f2210579df4bcb
yukikino <yuki.kinoa@gmail.com>
2014-03-28 22:12:20 +0900

    美容室登録画面作成

--- html/beauty_salon/config.php
+++ html/beauty_salon/config.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 
 ini_set('display_errors',1);
@@ -19,47 +19,3 @@ if (!function_exists('imagecreatetruecolor')) {
 	exit;
 }
 
-=======
-<<<<<<< HEAD
-<?php
-
-ini_set('display_errors',1);
-define('IMAGE_DIR','/var/www/html/uploads');
-define('THUMBNAIL_DIR','/var/www/html/thumbnails');
-define('THUMBNAIL_WIDTH', 72);
-
-define('MAX_FILE_SIZE', 1024000);
-
-
-//error_reporting ('E_ALL | E_STRICT');
-
-
-
-//GD
-if (!function_exists('imagecreatetruecolor')) {
-	echo 'GDがインストールされていない';
-	exit;
-}
-
-=======
-<?php
-
-ini_set('display_errors',1);
-define('IMAGE_DIR','/var/www/html/uploads');
-define('THUMBNAIL_DIR','/var/www/html/thumbnails');
-define('THUMBNAIL_WIDTH', 72);
-
-define('MAX_FILE_SIZE', 1024000);
-
-//error_reporting ('E_ALL | E_STRICT');
-
-
-//GD
-if (!function_exists('imagecreatetruecolor')) {
-	echo 'GDがインストールされていない';
-	exit;
-}
-
->>>>>>> 642068f6bc18e761c98f54ab644e833a40047b61
->>>>>>> a737cf711deca5fbb83e963f8c96da845b492e6b
-?>
